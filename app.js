592d2c15b4e8f260b7975a124ece0b50e84a579d
Ruslan Belyi <bely.ruslan@gmail.com>
2021-04-12 22:31:16 +0300

    Краш тест

--- app.js
+++ app.js
@@ -33,6 +33,12 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 
 app.use(express.json());
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signin', validateSignIn, login);
 
 app.post('/signup', validateSignOut, createUser);
