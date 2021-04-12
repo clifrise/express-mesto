f013c4f25f7baf96cbd88a8940f469dcbf658f50
Ruslan Belyi <bely.ruslan@gmail.com>
2021-04-12 12:18:57 +0300

    Подключение логов

--- app.js
+++ app.js
@@ -5,12 +5,15 @@ const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const { errorHandler } = require('./middlewares/error-handler');
 const { validateSignIn, validateSignOut } = require('./middlewares/validators');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const router = require('./routes');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+app.use(requestLogger);
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
@@ -28,6 +31,8 @@ app.post('/signup', validateSignOut, createUser);
 app.use(auth);
 
 app.use(router);
+
+app.use(errorLogger);
 
 app.use(errors());
 
